a7053636c6d58e4ae8cfb6bf78bcad690e6492fd
Edwin Roesli <eddie.roesli@gmail.com>
2017-10-16 10:45:36 +1300

    Edited Comments

--- src/tatai/UserHandler.java
+++ src/tatai/UserHandler.java
@@ -19,10 +19,25 @@ import javax.swing.JOptionPane;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
+
+/**
+*
+*Purpose of this class is to create a new JFrame with using the 
+*singleton design pattern and handle all user inputs including 
+*errors when inputting information.
+*
+*@author Edwin Roesli & Harpreet Singh
+*
+*/
+
+
 public class UserHandler {
 	
 	private static final Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
 	private static JFrame frame = null;
+	
+	
+	//Initialises the JFrame for signing up, made it into a singleton to avoid multiple signUp frames
 	
 	public static void signUpFrame() {
 		
@@ -30,6 +45,7 @@ public class UserHandler {
 			return;
 		}
 		
+		// Set up the frame
 		frame = new JFrame("New User");
 		frame.setSize(500, 700);
 		frame.setLocation(dim.width/2-frame.getSize().width/2, dim.height/2-frame.getSize().height/2);
@@ -39,12 +55,13 @@ public class UserHandler {
 		
 		
 		
-		
+		//Set up the mian title
 		JLabel lblNewUser = new JLabel("New User");
 		lblNewUser.setFont(new Font("DejaVu Sans", Font.BOLD, 40));
 		frame.getContentPane().add(lblNewUser);
 		
 		
+		//Sets up the "FullName" label along with its editable JTextField
 		JLabel lblFullName = new JLabel("Full Name:");
 		lblFullName.setFont(new Font("DejaVu Sans", Font.PLAIN, 20));
 		frame.getContentPane().add(lblFullName);
@@ -55,6 +72,7 @@ public class UserHandler {
 		
 		
 		
+		//Sets up the "Username" label along with its editable JTextField
 		JLabel lblUserName = new JLabel("Username:");
 		lblUserName.setFont(new Font("DejaVu Sans", Font.PLAIN, 20));
 		frame.getContentPane().add(lblUserName);
@@ -65,6 +83,7 @@ public class UserHandler {
 		
 		
 		
+		//Sets up the "Password" label along with its editable JPasswordField
 		JLabel lblPassWord = new JLabel("Password:");
 		lblPassWord.setFont(new Font("DejaVu Sans", Font.PLAIN, 20));
 		frame.getContentPane().add(lblPassWord);
@@ -75,6 +94,8 @@ public class UserHandler {
 		
 		
 		
+		//Sets up the "Confirm Password" label along with its editable JPasswordField
+		//Added this label to ensure the user has typed up the desired password correctly
 		JLabel lblConfirmPassWord = new JLabel("Confirm Password:");
 		lblConfirmPassWord.setFont(new Font("DejaVu Sans", Font.PLAIN, 20));
 		frame.getContentPane().add(lblConfirmPassWord);
@@ -84,14 +105,17 @@ public class UserHandler {
 		frame.getContentPane().add(txtConfirmPassword);
 		
 		
-		
+		//Sets up the "Create User" button 
 		JButton btnCreateUser = new JButton("Create User");
+		
+		//Adds an action listener 
 		btnCreateUser.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				
+				//checks whether creating the new user is valid or not
 				boolean validUserName = true;
 				
-				
+				//Cannot have spaces in the username
 				for ( int i = 0; i < txtUsername.getText().length(); i++) {
 					if (txtUsername.getText().charAt(i) == ' ' ) {
 						
@@ -100,20 +124,26 @@ public class UserHandler {
 					}
 				}
 				
-			
+				
+				//Checks if all text fields are fileld in 
 				if ((txtConfirmPassword.getPassword().length == 0)  || (txtFullName.getText().isEmpty()) || (txtPassword.getPassword().length == 0) || (txtUsername.getText().isEmpty())) {
 					JOptionPane.showMessageDialog(null, "Some fields not filled in");
 				}
 				
+				//Username cannot have spaces
 				else if (validUserName == false) {
 					JOptionPane.showMessageDialog(null, "Not a valid UserName, no spaces allowed");
 				}
 				
+				//Checks if the 2 passowrd fields are the same
 				else if (!(Arrays.equals(txtPassword.getPassword(), txtConfirmPassword.getPassword()))) {
 					
 					JOptionPane.showMessageDialog(null, "Password fields do not match");
 					
 				}
+				
+				//If all conditions are met, application tries to create the user
+				//If user already exists, returns with a error message dialogue
 				else {
 					System.out.println(txtPassword.getPassword().toString());
 					BashCommands commands = BashCommands.getInstance();
@@ -127,16 +157,22 @@ public class UserHandler {
 		frame.getContentPane().add(btnCreateUser);
 		
 		
+		//Sets the cancel button in the createUser gui 
 		JButton btnCancel = new JButton("Cancel");
+		
+		//Adds an action listener
 		btnCancel.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-			
-				int YesOrNo = JOptionPane.showConfirmDialog(null, "Are you sure you want to Cancel?","Cancel", JOptionPane.YES_NO_OPTION);
 				
 				
+				//Sets up an option dialogue to confirm your choice to cancel 
+				int YesOrNo = JOptionPane.showConfirmDialog(null, "Are you sure you want to Exit?","Exit", JOptionPane.YES_NO_OPTION);
+				
+				//if no, option dialogue goes away
 				if (YesOrNo == 1) {
 					return;
-					
+				
+				//if exit, exits out of the Jframe and back into the main GUI frame  
 				} else if (YesOrNo == 0)  {
 					frame.dispose();
 					frame = null;
@@ -150,7 +186,7 @@ public class UserHandler {
 		
 		frame.getContentPane().add(btnCancel);
 		
-		
+		//Sets all positions of the labels, buttons and TextFields
 		frame.getContentPane().addComponentListener(new ComponentAdapter() {
 			public void componentResized(ComponentEvent e) {
 				Component c = (Component)e.getSource();
@@ -179,6 +215,9 @@ public class UserHandler {
 		
 		frame.addWindowListener(new WindowAdapter()
         {
+		
+	// If window is closing, must set frame field to null or else when prompted to 
+	//Open "SignUp" again it won't allow it.
             @Override
             public void windowClosing(WindowEvent e)
             {
